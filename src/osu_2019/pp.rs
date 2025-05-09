565f9d610cfac3c709abd69631283d00ddb2e69a
Rafi <82166531+Xifted@users.noreply.github.com>
2025-05-09 16:01:52 +0700

    Update pp.rs

--- src/osu_2019/pp.rs
+++ src/osu_2019/pp.rs
@@ -504,7 +504,7 @@ impl<'m> OsuPP<'m> {
     fn calculate_miss_penalty(&self, effective_miss_count: f32) -> f32 {
         let total_hits = self.total_hits() as f32;
 
-        0.98 * (1.0 - (effective_miss_count / total_hits).powf(0.5))
+        let base_penalty = 0.98 * (1.0 - (effective_miss_count / total_hits).powf(0.5))
             .powf(1.0 + effective_miss_count);
         
         base_penalty - (effective_miss_count * 0.01)
