f1b2f058c6b360a3298e53c0108a4718249beade
ImOyZo <96724899+ImOyZo@users.noreply.github.com>
2025-06-03 18:57:53 +0800

    Meh

--- src/osu/pp.rs
+++ src/osu/pp.rs
@@ -564,15 +564,15 @@ impl OsuPpInner {
             return 0.0;
         }
 
+        // * EZAP NERF
+        if self.mods.ap() && self.mods.ez() {
+            return 0.5
+        };
+
         let mut speed_value = if self.mods.ap() {
             (5.4 * (self.attrs.speed / 0.0675).max(1.0) - 4.0).powi(3) / 100_000.0
         } else {
             (5.0 * (self.attrs.speed / 0.0675).max(1.0) - 4.0).powi(3) / 100_000.0
-        };
-        
-        // * EZAP NERF
-        speed_value *= if self.mods.ap() && self.mods.ez() {
-            0.5
         };
 
         let total_hits = self.total_hits();
